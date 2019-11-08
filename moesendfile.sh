32bae7c63da9ac81950e4e19256429f803052d2b
Akkariin Meiko <akkariins@gmail.com>
2019-11-09 06:36:34 +0800

    Update moesendfile.sh

--- moesendfile.sh
+++ moesendfile.sh
@@ -2,5 +2,5 @@
 API="http://t.mcr.moe:812"
 # 注释掉上面，改为下面这个即可使用 https 加密传输
 # API="https://t.mcr.moe"
-cat $2 | base64 | curl -s "${API}/send/$1" > /dev/null -d @-
+cat $2 | base64 | curl -s "${API}/send/$1" -d @- > /dev/null
 curl -s "${API}/close/$1" > /dev/null
